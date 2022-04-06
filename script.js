f74edcb5ee124df52f4d21addcfa8d70a34fd768
augustolfp <augustolfp@gmail.com>
2022-04-05 22:59:54 -0300

    Implementação de função que pergunta número de cartas ao jogador

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 
+function PerguntaQuantasCartas() {
+    let NumeroDeCartas = prompt("Com quantas Cartas você deseja jogar?");
+    if(NumeroDeCartas % 2 === 0 && NumeroDeCartas >= 4 && NumeroDeCartas <= 14) {
+        return NumeroDeCartas;
+    }
+    else {
+        alert("Esse é um jogo da memória, o número de cartas precisa ser par!\n\nAlém disso, o número mínimo são 4 cartas, e o máximo 14!");
+        PerguntaQuantasCartas();
+    }
+}
+
+PerguntaQuantasCartas();
