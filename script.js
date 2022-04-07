510ce7d7a628fba6b2db7af0116f5a4a1c3f0573
augustolfp <augustolfp@gmail.com>
2022-04-07 16:03:50 -0300

    Implementação de função que insere X cartas na tela

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 
-function PerguntaQuantasCartas() {
-    let NumeroDeCartas = prompt("Com quantas Cartas você deseja jogar?");
-    if(NumeroDeCartas % 2 === 0 && NumeroDeCartas >= 4 && NumeroDeCartas <= 14) {
-        return NumeroDeCartas;
+function perguntaQuantasCartas() {
+    let numeroDeCartas = prompt("Com quantas Cartas você deseja jogar?");
+    if(numeroDeCartas % 2 === 0 && numeroDeCartas >= 4 && numeroDeCartas <= 14) {
+        return numeroDeCartas;
     }
     else {
         alert("Esse é um jogo da memória, o número de cartas precisa ser par!\n\nAlém disso, o número mínimo são 4 cartas, e o máximo 14!");
-        PerguntaQuantasCartas();
+        perguntaQuantasCartas();
     }
 }
 
-PerguntaQuantasCartas();
+function insereCartasNaTela() {
+    let numeroDeCartas = perguntaQuantasCartas();
+    for(let i=0;i<numeroDeCartas;i++) {
+        let containerCartas = document.querySelector(".containerCartas");
+        let umaCarta = "<div class='Carta'><img src='Pngs/front.png'></div>";
+        containerCartas.innerHTML = containerCartas.innerHTML + umaCarta;
+    }
+}
+
+insereCartasNaTela();
