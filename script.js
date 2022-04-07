833892901aa14399a02c6653adf901c1f92068d1
augustolfp <augustolfp@gmail.com>
2022-04-07 17:27:01 -0300

    Implementação de funções que revelam e escondem cartas

--- script.js
+++ script.js
@@ -14,9 +14,27 @@ function insereCartasNaTela() {
     let numeroDeCartas = perguntaQuantasCartas();
     for(let i=0;i<numeroDeCartas;i++) {
         let containerCartas = document.querySelector(".containerCartas");
-        let umaCarta = "<div class='Carta'><img src='Pngs/front.png'></div>";
+        let umaCarta = "<div class='Carta' onclick='revelaCartaClicada(this)'><img class='fundoCarta' src='Pngs/front.png'><img class='frenteCarta Escondido' src='Gifs/bobrossparrot.gif'></div>";
         containerCartas.innerHTML = containerCartas.innerHTML + umaCarta;
     }
 }
 
-insereCartasNaTela();
+
+
+function revelaCartaClicada(cartaClicada) {
+    let fundoCarta = cartaClicada.querySelector(".fundoCarta");
+    let frenteCarta = cartaClicada.querySelector(".frenteCarta");
+    fundoCarta.classList.add("Escondido");
+    frenteCarta.classList.remove("Escondido");
+}
+
+function escondeTodasCartas() {
+    let fundoCarta = document.querySelectorAll(".fundoCarta");
+    let frenteCarta = document.querySelectorAll(".frenteCarta");
+    for(let i=0; i<fundoCarta.length; i++) {
+        fundoCarta[i].classList.remove("Escondido");
+        frenteCarta[i].classList.add("Escondido");
+    }
+}
+
+insereCartasNaTela()
